0137720bd9d13620ad7f043a1275b1f53a920a2f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-12 16:53:26 +0700

    update-link-container

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div>AboutUs</div>
+  )
+}
+
+export default AboutUs
---
+++ src/container/Laurels/Laurels.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Laurels.css";
+
+const Laurels = () => {
+  return (
+    <div>Laurels</div>
+  )
+}
+
+export default Laurels
