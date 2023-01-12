8c9f7fbf7773da3bf3d3e564bc2fc87f34ae6dec
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-12 18:08:24 +0700

    update-fix-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
