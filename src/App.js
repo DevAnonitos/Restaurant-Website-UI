400ba55339a626e9ab5e49449cece15b18f6b667
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-13 20:59:14 +0700

    update-state-hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
+function Loading() {
+  return <h2>🌀 Loading...</h2>;
+}
+
 const App = () => (
   <>
-    <div>
-      <Navbar />
-      <Header />
-      <AboutUs />
-      <SpecialMenu />
-      <Chef />
-      <Intro />
-      <Laurels />
-      <Gallery />
-      <FindUs />
-      <Footer />
-    </div>    
+    <Suspense fallback={<Loading />}>
+      <div>
+        <Navbar />
+        <Header />
+        <AboutUs />
+        <SpecialMenu />
+        <Chef />
+        <Intro />
+        <Laurels />
+        <Gallery />
+        <FindUs />
+        <Footer />
+      </div>    
+    </Suspense>
   </>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Suspense }from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Loading() {
+  return <h2>🌀 Loading...</h2>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<Loading />}>    
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
 
