5ff59bece32068ca4389d3c96ccb8ae2c578e8d5
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-15 17:38:44 +0700

    update-Awards

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,52 @@
+
 import React from 'react';
-import "./Laurels.css";
 
-const Laurels = () => {
-  return (
-    <div>Laurels</div>
-  )
-}
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
+import './Laurels.css';
 
-export default Laurels
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img 
+      src={imgUrl} 
+      alt="awards" 
+    />
+    <div className="app__laurels_awards-card_content">
+      <p 
+        className="p__cormorant" 
+        style={{ color: '#DCCA87' }}
+      >
+        {title}
+      </p>
+      <p className="p__opensans">
+        {subtitle}
+      </p>
+    </div>
+  </div>
+);
+
+const Laurels = () => (
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">
+        Our Laurels
+      </h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => 
+          <AwardCard award={award} key={award.title} />
+        )}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img 
+        src={images.laurels} 
+        alt="laurels_img" 
+      />
+    </div>
+  </div>
+);
+
+export default Laurels;
